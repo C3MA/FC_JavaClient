338f379c1b3aeb8d6e8072e421b5f9469f9df7cc
justsly <b.brixner@t-online.de>
2013-08-03 00:29:20 +0200

    Spielfortschritt eingebaut

--- example/de/c3ma/fullcircle/client/ExampleInputStation.java
+++ example/de/c3ma/fullcircle/client/ExampleInputStation.java
@@ -10,8 +10,10 @@ import java.util.Random;
 
 import javax.swing.JFrame;
 
+import de.c3ma.animation.RainbowEllipse;
 import de.c3ma.fullcircle.dyn.Dynamic;
 import de.c3ma.fullcircle.dyn.OnFullcirclePaint;
+import de.c3ma.types.SimpleColor;
 
 /**
  * created at 22.10.2012 - 22:06:12<br />
@@ -28,10 +30,10 @@ public class ExampleInputStation extends JFrame implements OnFullcirclePaint, Ke
      * Unique Id that is needed for the JFrame
      */
     private static final long serialVersionUID = 191520503239122366L;
-    
-    private int frame_index = 0;
-    
-    private int x_pos, y_pos, x_enemy, y_enemy, height, width;
+        
+    private int x_pos, y_pos, x_enemy, y_enemy, height, width, x_stat, stat;
+
+	private int count;
     
     private static final int SIZE = 2;
     
@@ -58,29 +60,47 @@ public class ExampleInputStation extends JFrame implements OnFullcirclePaint, Ke
     }
 
     @Override
-    public void paint(Graphics g, int width, int height) {
+    public void paint(final Graphics g, int width, int height) {
     	if(this.height==0 && this.width==0){
     		this.height = height;
         	this.width = width;
         	this.x_pos = width/2;
         	this.y_pos = height-SIZE;
         	this.y_enemy = height+1;
+        	this.x_stat = 1;
+        	this.stat=1;
     	}
-            frame_index = 0;
         
         
         boolean hit = this.enemy_pos();
         if(hit)
         {
         	g.setColor(Color.orange);
-        	g.fillRect(0,0,width,height);
+        	new RainbowEllipse(width/2, height/2, width/2, height/2) {
+
+                @Override
+                protected void drawPixel(int x, int y, SimpleColor c) {
+                	Color c2 = new Color(c.getRed(), c.getGreen(), c.getBlue());
+					g.setColor(c2);
+                    g.fillRect(x, y, 1, 1);
+                }
+                
+            }.drawEllipse(count++);
         }
         else
         {
+        	
+        g.setColor(new Color(0, stat,0));
+        g.fillRect(0, 0 , x_stat, 1);
         g.setColor(Color.blue);
         g.fillRect(x_pos, y_pos, SIZE, SIZE);
         g.setColor(Color.red);
         g.fillRect(x_enemy, y_enemy, 1, 1);
+        stat+=5;
+        if(stat>=255){
+        	stat=0;
+        	x_stat++;
+        }
         }
     } 
 
@@ -110,6 +130,8 @@ public class ExampleInputStation extends JFrame implements OnFullcirclePaint, Ke
         else if(e.getKeyCode() == KeyEvent.VK_SPACE)
         {
         	y_enemy=height+1;
+        	x_stat=1;
+        	stat=1;
         }
         else {
             System.out.println("KEY : " + e.getID() + " [expected=" + Event.KEY_ACTION + "] "+ e.getKeyCode());
